71a6a2a808f93e9ffb120e62b063a63a288b6249
tahmedofficial <tanvirofcuo@gmail.com>
2024-09-26 00:43:56 +0600

    added service api

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const app = express();
-const prot = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-// Middlewares
-app.use(cors());
+// middleware
+app.use(cors({ origin: ["http://localhost:5173",], credentials: true, }));
 app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ufkobjs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -23,23 +22,50 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+
+        const database = client.db("carDoctorDB");
+        const serviceCollection = database.collection("services");
+        const bookingCollection = database.collection("bookings");
+
+        // Code hear-------------------------------
+
+        app.get("/services", async (req, res) => {
+            const cursor = serviceCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get("/services/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const options = {
+                projection: { title: 1, price: 1, service_id: 1 }
+            }
+            const result = await serviceCollection.findOne(query, options);
+            res.send(result);
+        })
+
+        // Bookings
+        app.post("/bookings", async (req, res) => {
+            const booking = req.body;
+
+        })
+
+        // Code hear-------------------------------
+
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
-    res.send("server is running");
+    res.send("Server is running");
 })
 
-app.listen(prot, () => {
-    console.log(`server is running on port ${prot}`);
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
 })
