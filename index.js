c0c4d6a10dd535f89a29c5a092a60656831553d2
tahmedofficial <tanvirofcuo@gmail.com>
2024-09-26 22:55:12 +0600

    added delete and post api

--- index.js
+++ index.js
@@ -39,16 +39,35 @@ async function run() {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const options = {
-                projection: { title: 1, price: 1, service_id: 1 }
+                projection: { title: 1, price: 1, service_id: 1, img: 1 }
             }
             const result = await serviceCollection.findOne(query, options);
             res.send(result);
         })
 
         // Bookings
+        app.get("/bookings", async (req, res) => {
+            console.log(req.query);
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email };
+            }
+            const result = await bookingCollection.find(query).toArray();
+            res.send(result);
+        })
+
         app.post("/bookings", async (req, res) => {
             const booking = req.body;
+            console.log(booking);
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+        })
 
+        app.delete("/bookings/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result);
         })
 
         // Code hear-------------------------------
