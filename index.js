eda4cbf11bd9514c5be9934582d21ccf8226545d
tahmedofficial <tanvirofcuo@gmail.com>
2024-09-29 20:53:58 +0600

    finish jwt token setup

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -8,6 +10,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors({ origin: ["http://localhost:5173",], credentials: true, }));
 app.use(express.json());
+app.use(cookieParser());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ufkobjs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -27,8 +30,23 @@ async function run() {
         const serviceCollection = database.collection("services");
         const bookingCollection = database.collection("bookings");
 
+        const cookieOptions = {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production" ? true : false,
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        };
+
         // Code hear-------------------------------
 
+        // Auth related api
+        app.post("/jwt", async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const token = jwt.sign(user, process.env.SECRET_TOKEN, { expiresIn: "1h" });
+            res.cookie("token", token, cookieOptions).send({ success: true });
+        })
+
+        // services related api
         app.get("/services", async (req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
@@ -48,6 +66,7 @@ async function run() {
         // Bookings
         app.get("/bookings", async (req, res) => {
             console.log(req.query);
+            console.log("token ",req.cookies.token);
             let query = {};
             if (req.query?.email) {
                 query = { email: req.query.email };
